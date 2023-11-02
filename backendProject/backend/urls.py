1150945a30c86f4ee512eac4b6c4e5ebc48b52b0
Zhi <neozhixuan@gmail.com>
2023-11-03 02:24:52 +0800

    Feature - POST URLs for REST

--- backendProject/backend/urls.py
+++ backendProject/backend/urls.py
@@ -8,6 +8,9 @@ urlpatterns = [
     path('fn/expiringFoods', views.filterForLowStock, name="filterForLowStock"),
     path('fn/getItemNames', views.getItemNames, name="getItemNames"),
     path('fn/createOrderData/', views.createOrderData, name='createOrderData'),
+    path('fn/createSuggestion/', views.createSuggestion, name='createSuggestions'),
+    path('fn/createPrediction/', views.createPrediction, name='createPredictions'),
+    path('fn/createSupplier/', views.createSupplier, name='createSupplier'),
 
     # REST API URLS
     path('api/orderdata/', views.OrderDataList.as_view(), name='orderdata-list'),
